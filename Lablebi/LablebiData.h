f0fb7554f110cc37615522744f682d90d3ae59b3
salimos <socom.skx@gmail.com>
2013-05-05 16:28:02 +0100

    Ajout de la 1ére classe

---
+++ Lablebi/LablebiData.h
@@ -0,0 +1,20 @@
+//
+//  LablebiData.h
+//  Lablebi
+//
+//  Created by Slim Khan on 05/05/13.
+//  Copyright (c) 2013 Slim Khan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LablebiData : NSObject
+
+@property (strong) NSString *title;
+
+@property (assign) float La;
+@property (assign) float Lg;
+
+
+
+@end
---
+++ Lablebi/LablebiData.m
@@ -0,0 +1,13 @@
+//
+//  LablebiData.m
+//  Lablebi
+//
+//  Created by Slim Khan on 05/05/13.
+//  Copyright (c) 2013 Slim Khan. All rights reserved.
+//
+
+#import "LablebiData.h"
+
+@implementation LablebiData
+
+@end
